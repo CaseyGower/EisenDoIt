afb8b4577a9240d71bd1ec8e591a608e36cb9924
Casey Gower <caseyg@charter.net>
2016-12-11 16:31:50 -0600

    Connect DAD style sheet, Change input box type

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -3,12 +3,12 @@ var main = function () {
     // when button is clicked...
     $("#submitButton").click(function (event) {
         // get new comment text from input box
-        var inputText = $("#input").val();
+        var inputText = $("#nweToDo").val();
 
-        var newToDo = $("#box1");
+        var newToDo = $("<p>");
         newToDo.text(inputText);
 
-        newToDo.hide();
+
         $("#box1").append(newToDo);
 
         $("#newCommentBox").val("");
